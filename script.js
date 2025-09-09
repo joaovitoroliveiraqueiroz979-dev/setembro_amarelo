513d29c2e32838844c8e77b03b5aa2bd7e2781a0
joaovitoroliveiraqueiroz979-dev <joaovitoroliveiraqueiroz979@gmail.com>
2025-09-09 14:29:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const mensagens = [
-  "Você é mais forte do que imagina ಠ_ಠ",
-  "Nunca se esqueça: sua vida importa (¬_¬ )",
-  "Você não está sozinho, estamos juntos ╯︿╰",
-  "Acredite: dias melhores virão ᕦ(ò_óˇ)ᕤ",
-  "Sempre existe esperança 💖",
+  "Você é mais forte do que imagina (❤´艸｀❤)",
+  "Nunca se esqueça: sua vida importa (❤ ω ❤)",
+  "Você não está sozinho, estamos juntos (´▽`ʃ♡ƪ)",
+  "Acredite: dias melhores virão (✿◡‿◡)",
+  "Sempre existe esperança 💖"
 ];
 
 document.getElementById("btnMensagem").addEventListener("click", () => {
